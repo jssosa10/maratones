568f7a9ff022f92ddf745807c79914e570f24bef
Juan Sosa <js.sosa10@uniandes.edu.co>
2017-09-01 22:29:40 +0000

    journey

---
+++ journey.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 100005;
+vector<int> g[MAX];
+bool is[MAX];
+double res;
+int n,x,y;
+void DFS(int i,double p, int d){
+    if(g[i].size()>0){
+        int vis = 0;
+        int s = g[i].size();
+        s = (i==1)?s:s-1;
+        if(s){
+        is[i]=true;
+            for (vector<int>::iterator it = g[i].begin(); it != g[i].end(); ++it) {
+                if(!is[*it]){
+                    DFS(*it,(p/s),d+1);
+                    vis++;
+                }
+            }
+        }
+        else
+            res += p*d;
+        
+    }
+}
+int main(){
+    memset(is,false,MAX-1);
+    scanf("%d",&n);
+        int m=n-1;
+        while(m--){
+            scanf("%d %d",&x,&y);
+            g[x].push_back(y);
+            g[y].push_back(x);
+        }
+        DFS(1,1.0,0);
+    printf("%.9f",res);
+    return 0;
+}
--- oddsends.cpp
+++ oddsends.cpp
@@ -14,17 +14,7 @@ int main(){
         }
         if(vals[1]%2==0||vals[n]%2==0)printf("No\n");
         else{
-          for (int i = 1; i < n; i = i+2) {
-              if(vals[i]%2!=0 && vals[i+1]%2!=0){
-                  ct++;
-                  i--;
-              }
-          }
-          ct++;
-          if(ct%2==0){
-              printf("No\n");
-          }
-          else printf("Yes\n");
+            printf("Yes\n");
         }
     }
     return 0;
